2980d60ed6d954f71625a959fe26cb1b59a98359
NISZOgen <niszogen@niszogen.com>
2024-11-07 14:32:17 +0100

    colors update :D

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { inject } from "@vercel/analytics";
 inject();
 
-import "./assets/style.css";
+import "./assets/style.scss";
+import "./assets/buttons.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
